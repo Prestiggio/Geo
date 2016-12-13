fd2e121932c7b42f6538693cf60e4ccb1503ce50
Tsikawa Topgasy <topgasy@tsikawa.com>
2016-12-13 14:44:23 +0200

    friendly address

--- src/Models/Traits/Geoable.php
+++ src/Models/Traits/Geoable.php
@@ -4,6 +4,10 @@ namespace Ry\Geo\Models\Traits;
 trait Geoable
 {
 	public function address() {
-		return $this->belongsTo("Ry\Geo\Models\Adresse", "address_id");
+		return $this->belongsTo("Ry\Geo\Models\Adresse", "adresse_id");
+	}
+	
+	public function getCompleteAddressAttribute() {
+		return $this->address->raw . "<br/>" . $this->address->ville->cp . " " . $this->address->ville->nom . "<br/>" . $this->address->ville->country->nom;
 	}
 }
