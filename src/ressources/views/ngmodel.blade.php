e361493cd63dc81dd169607aa35764ce537b3a65
Top Mora <manager@topmora.com>
2017-06-15 18:29:35 +0200

    Kipa -  geo editable

--- src/ressources/views/ngmodel.blade.php
+++ src/ressources/views/ngmodel.blade.php
@@ -5,10 +5,10 @@
 	</md-input-container>
 	<md-input-container>
 		<label>Adresse</label>
-		<input type="text" name="immobilier_adresse_raw" ng-model="data.raw"/>
+		<input type="text" name="adresse_raw" ng-model="data.raw"/>
 	</md-input-container>
 	<md-input-container>
-		<md-button class="md-icon-button" ng-click="rediger('data')" aria-label="Questionner l'auteur"><md-icon md-font-icon="fa fa-send"></md-icon></md-button>
+		<md-button class="md-icon-button" ng-click="$root.ask('latlng')" aria-label="Questionner l'auteur"><md-icon md-font-icon="fa fa-send"></md-icon></md-button>
 	</md-input-container>
 </div>
 <div layout="row">
@@ -17,7 +17,7 @@
 	</md-input-container>
 	<md-input-container>
 		<label>Ville</label>
-		<input type="text" name="immobilier_adresse_ville_nom" ng-model="data.ville.nom"/>
+		<input type="text" name="adresse_ville_nom" ng-model="data.ville.nom"/>
 	</md-input-container>
 </div>
 <div layout="row">
@@ -26,7 +26,7 @@
 	</md-input-container>
 	<md-input-container>
 		<label>Pays</label>
-		<input type="text" name="immobilier_adresse_ville_country_nom" ng-model="data.ville.country.nom"/>
+		<input type="text" name="adresse_ville_country_nom" ng-model="data.ville.country.nom"/>
 	</md-input-container>
 </div>
 <rymap ng-model="data" style="min-height:600px;"></rymap>
