9fba54c5df20f5bf0104dce9e4ba8b282e117aa5
Landr Rakotoarivelo <folojona@gmail.com>
2019-03-01 17:33:44 +0200

    fix big space

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -36,50 +36,50 @@ class PublicController extends Controller
 			$country = false;
 			$ville = false;
 			if(isset($ar["ville"]["country"]["id"]) && $ar["ville"]["country"]["id"] > 0) {
-				$country = Country::where("id", "=", $ar["ville"]["country"]["id"])->first();
+				$country = Country::where("id","=",$ar["ville"]["country"]["id"])->first();
 			}
-			elseif(isset($ar  ["ville"] ["country"] ["nom"])){
-				$country = Country::where ( "nom", "LIKE", $ar  ["ville"] ["country"] ["nom"] )->first ();
+			elseif(isset($ar["ville"]["country"]["nom"])){
+				$country = Country::where("nom","LIKE",$ar["ville"]["country"]["nom"])->first();
 			}
 			
 			Ville::unguard();
 			if (!$country) {
-				$country = new Country ();
-				$country->nom = $ar  ["ville"] ["country"] ["nom"];
+				$country = new Country();
+				$country->nom = $ar["ville"]["country"]["nom"];
 				$country->save ();
 				
-				$ville = $country->villes ()->create ( [
-						"nom" => $ar  ["ville"] ["nom"],
-						"cp" => isset($ar  ["ville"] ["cp"]) ? $ar  ["ville"] ["cp"] : ""
-				] );
+				$ville = $country->villes()->create([
+						"nom" => $ar["ville"]["nom"],
+						"cp" => isset($ar["ville"]["cp"]) ? $ar["ville"]["cp"] : ""
+				]);
 			}
 			else {
 				if(isset($ar["ville"]["id"]) && $ar["ville"]["id"]>0) {
-					$ville = $country->villes ()->where ( "id", "=", $ar  ["ville"] ["id"] )
-					->where("nom", "LIKE", $ar  ["ville"] ["nom"])->first ();
+					$ville = $country->villes()->where( "id", "=", $ar["ville"]["id"])
+					->where("nom", "LIKE", $ar["ville"]["nom"])->first ();
 				}
-				elseif(isset($ar  ["ville"] ["nom"])) {
-					$ville = $country->villes ()->where ( "nom", "LIKE", $ar  ["ville"] ["nom"] )->first ();
+				elseif(isset($ar["ville"]["nom"])) {
+					$ville = $country->villes()->where("nom","LIKE",$ar["ville"]["nom"])->first();
 				}
 				
-				if (! $ville) {
-					$ville = $country->villes ()->create ( [
-							"nom" => $ar  ["ville"] ["nom"],
-							"cp" => isset($ar  ["ville"] ["cp"]) ? $ar  ["ville"] ["cp"] : ""
-					] );
+				if (!$ville) {
+					$ville = $country->villes()->create ([
+							"nom" => $ar["ville"]["nom"],
+							"cp" => isset($ar["ville"]["cp"]) ? $ar["ville"]["cp"] : ""
+					]);
 				}
 			}	
 			Ville::reguard();	
-			$adresse = new Adresse ();
+			$adresse = new Adresse();
 			$adresse->ville_id = $ville->id;
 		}
 		
-		$adresse->raw = $ar  ["raw"];
+		$adresse->raw = $ar["raw"];
 		$adresse->raw2 = isset($ar["raw2"]) ? $ar["raw2"] : null;
 		$adresse->lat = isset($ar["lat"]) ? $ar["lat"] : "-18.913396429147";
 		$adresse->lng = isset($ar["lng"]) ? $ar["lng"] : "47.521104812622";
 		
-		$adresse->save ();
+		$adresse->save();
 		
 		return $adresse;
 	}
