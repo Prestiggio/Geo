b406275845ffbf2d4ea269168caf951833c954d3
Landr Rakotoarivelo <folojona@gmail.com>
2019-10-02 14:52:26 +0200

    update country

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -32,48 +32,17 @@ class PublicController extends Controller
 	    
 		$adresse = Adresse::where("raw", "LIKE", $ar["raw"])->first();
 		
-		if(!$adresse) {			
-			$country = false;
-			$ville = false;
-			if(isset($ar["ville"]["country"]["id"]) && $ar["ville"]["country"]["id"] > 0) {
-				$country = Country::where("id","=",$ar["ville"]["country"]["id"])->first();
-			}
-			elseif(isset($ar["ville"]["country"]["nom"])){
-				$country = Country::where("nom","LIKE",$ar["ville"]["country"]["nom"])->first();
-			}
-			
-			Ville::unguard();
-			if (!$country) {
-				$country = new Country();
-				$country->nom = $ar["ville"]["country"]["nom"];
-				$country->save ();
-				
-				$ville = $country->villes()->create([
-						"nom" => $ar["ville"]["nom"],
-						"cp" => isset($ar["ville"]["cp"]) ? $ar["ville"]["cp"] : ""
-				]);
-			}
-			else {
-				if(isset($ar["ville"]["id"]) && $ar["ville"]["id"]>0) {
-					$ville = $country->villes()->where( "id", "=", $ar["ville"]["id"])
-					->where("nom", "LIKE", $ar["ville"]["nom"])->first ();
-				}
-				elseif(isset($ar["ville"]["nom"])) {
-					$ville = $country->villes()->where("nom","LIKE",$ar["ville"]["nom"])->first();
-				}
-				
-				if (!$ville) {
-					$ville = $country->villes()->create ([
-							"nom" => $ar["ville"]["nom"],
-							"cp" => isset($ar["ville"]["cp"]) ? $ar["ville"]["cp"] : ""
-					]);
-				}
-			}	
-			Ville::reguard();	
+		if(!$adresse) {	
+		    if(!isset($ar['ville']))
+		        return;
 			$adresse = new Adresse();
-			$adresse->ville_id = $ville->id;
+			$ville = $this->generateVilleChanged($ar['ville']);
+		}
+		else {
+		    $ville = $this->generateVilleChanged($ar['ville'], (isset($ar['ville']['id']) && $ar['ville']['id'] != $adresse->ville_id)||(isset($ar['ville']['country']['id']) && $ar['ville']['country']['id'] != $adresse->ville->country_id));
 		}
 		
+		$adresse->ville_id = $ville->id;
 		$adresse->raw = $ar["raw"];
 		$adresse->raw2 = isset($ar["raw2"]) ? $ar["raw2"] : null;
 		$adresse->lat = isset($ar["lat"]) ? $ar["lat"] : "-18.913396429147";
@@ -82,6 +51,60 @@ class PublicController extends Controller
 		$adresse->save();
 		
 		return $adresse;
+	}
+	
+	private function generateVilleChanged($arville, $recreate=false) {	    
+	    if(!isset($arville['country']))
+	        return;
+	    
+	    $ville = false;
+	    $country = $this->generateCountryChanged($arville['country']);
+	    
+	    if($recreate) {
+	        Ville::unguard();
+	        $ville = $country->villes()->create([
+	            "nom" => $arville["nom"],
+	            "cp" => isset($arville["cp"]) ? $arville["cp"] : ""
+	        ]);
+	        Ville::reguard();
+	        return $ville;
+	    }
+	    
+	    if(isset($arville["id"]) && $arville["id"]>0) {
+	        $ville = $country->villes()->where( "id", "=", $arville["id"])->first();
+	    }
+	    elseif(isset($arville['nom'])) {
+	        $ville = $country->villes()->where("nom","LIKE",$arville["nom"])->first();
+	    }
+	    
+	    if(!$ville || ($ville && $ville->country_id != $country->id)) {
+	        if(!isset($arville["nom"]))
+	            return;
+	        
+	        Ville::unguard();
+	        $ville = $country->villes()->create([
+	            "nom" => $arville["nom"],
+	            "cp" => isset($arville["cp"]) ? $arville["cp"] : ""
+	        ]);
+	        Ville::reguard();
+	    }
+	    
+	    return $ville;
+	}
+	
+	private function generateCountryChanged($arcountry, $recreate=false) {
+	    if(isset($arcountry["id"]) && $arcountry["id"] > 0) {
+	        return Country::where("id","=",$arcountry["id"])->first();
+	    }
+	    elseif(isset($arcountry["nom"])){
+	        $country = Country::where("nom","LIKE",$arcountry["nom"])->first();
+	        if(!$country) {
+	            $country = new Country();
+	            $country->nom = $arcountry["nom"];
+	            $country->save ();
+	        }
+	        return $country;
+	    }
 	}
 }
 ?>
